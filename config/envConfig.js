20f7339f4c99c4da65c790e200f45f25e4116100
Konstantin <k0si0.lazarov@gmail.com>
2019-06-10 00:48:10 +0300

    Set database path.

--- config/envConfig.js
+++ config/envConfig.js
@@ -7,7 +7,7 @@ module.exports = {
         rootPath: path.normalize(path.join(__dirname, '/../'))
     },
     production: {
-        dbPath: process.env.DBPATH,
+        dbPath: 'mongodb://heroku_2x7j20dq:s4qg3qr7tojmorfhd67q1qvv67@ds235437.mlab.com:35437/heroku_2x7j20dq',
         port: process.env.PORT || 3000,
         rootPath: path.normalize(path.join(__dirname, '/../'))
     }
